80ba517ef9bb51f58798f33c1ac896c91a3534b6
Malhar Prasad Ray <malharprasadray@gmail.com>
2026-02-21 18:35:11 +0530

    auth completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar'
-import { useLocation } from 'react-router-dom'
+import { useLocation, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
 
 const App = () => {
 
@@ -8,6 +9,11 @@ const App = () => {
   return (
     <div>
       {!isOwnerPath &&  <Navbar />}
+      <div className='min-h-[70vh]'>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+      </Routes>
+      </div>
     </div>
   )
 }
