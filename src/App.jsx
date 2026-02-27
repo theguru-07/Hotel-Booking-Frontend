031fe0a3eb07670273a3af0915fb42dd5d4ad562
Malhar Prasad Ray <malharprasadray@gmail.com>
2026-02-27 09:38:44 +0530

    ui almost done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import Navbar from './components/Navbar'
 import { useLocation, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home';
 import Footer from './components/Footer';
+import AllRooms from './pages/AllRooms';
+import RoomDetails from './pages/RoomDetails';
 
 const App = () => {
 
@@ -13,6 +15,8 @@ const App = () => {
       <div className='min-h-[70vh]'>
       <Routes>
         <Route path='/' element={<Home/>} />
+        <Route path='/rooms' element={<AllRooms/>} />
+        <Route path='/rooms/:id' element={<RoomDetails/>} />
       </Routes>
       </div>
       <Footer />
