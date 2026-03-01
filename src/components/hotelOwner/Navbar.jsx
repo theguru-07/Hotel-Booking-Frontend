81d6502abf77c07553f3306116ec4a572029e036
Malhar Prasad Ray <malharprasadray@gmail.com>
2026-03-02 01:18:11 +0530

    admin part completed

--- src/components/hotelOwner/Navbar.jsx
+++ src/components/hotelOwner/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { assets } from '../../assets/assets'
+import { UserButton } from '@clerk/clerk-react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white transition-all duration-300'>
+      <Link to='/'>
+        <img src={assets.Nestigoo} alt="logo" className='h-9 invert opacity-80'/>
+      </Link>
+      <UserButton />
+    </div>
   )
 }
 
